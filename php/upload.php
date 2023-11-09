76dfad0fde10f9066a6a936aa88864b7fa0e72a7
Leonardo Criollo <criollo_banneret@tutanota.com>
2023-11-09 07:13:21 -0600

    Commit inicial

---
+++ php/upload.php
@@ -0,0 +1,6 @@
+<?php
+    $filename = $_FILEs['file']['name'];
+    $tmp_name = $_FILES['file']['tmp_name'];
+    $file_up_name = time() . $filename;
+    move_uploaded_file($tmp_name, "files/" . $file_up_name);
+?>
